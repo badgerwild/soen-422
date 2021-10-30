02a932b79d726fc59f868bc2f9629c468cea7c2a
badgerwild <jklimock@gmail.com>
2021-10-29 20:21:44 -0400

    Finish task 3

--- Lab_4/task_3/isr_timer.c
+++ Lab_4/task_3/isr_timer.c
@@ -6,12 +6,30 @@
 
 
 #define outLed 5
+#define F_CPU 16000000UL //16MHz cpu
+#define time 0.004 //nterrupts at this time 
 
+volatile uint16_t count = 0;
 ISR(TIMER0_COMPA_vect){
+    cli();
+        count += 4;
+        if (count == 500) {
+            PORTB ^= (1ul << outLed);
+           count = 0;
+        }
+    
 
+    sei();
 }
 
-while(true) {
+int main(){
+    DDRB |= (1 << outLed); 
+    TCCR0A |= (1 <<WGM01); 
+    TCCR0B |= (1 << 2); // set prescaler to 256 by turning on bit 2 in the TCCR0B register
+    TIMSK0 |= (1 << 1); //enable compare match register A with bit 1 
+    OCR0A = (F_CPU / 256)* time;
+    sei();
+    while(true) {
 
-
+    }
 }
